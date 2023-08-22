d2a2b91253155a609c88b11a30d0f134c91a2395
Alfonso Almonte <alfonsoalmonte@gmail.com>
2023-08-22 10:36:49 -0400

    Adding ServiceUrl Security microservice to eureka-server

--- src/main/java/s1014ftjavaangular/security/SecurityApplication.java
+++ src/main/java/s1014ftjavaangular/security/SecurityApplication.java
@@ -3,9 +3,14 @@ package s1014ftjavaangular.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaClientConfigBean;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Map;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,6 +18,14 @@ public class SecurityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SecurityApplication.class, args);
+    }
+
+    @Bean
+    public EurekaClientConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+        var config = new EurekaClientConfigBean();
+        config.setServiceUrl(Map.of("defaultZone", "https://s10-14-ft-eurekaserver.azurewebsites.net/eureka"));
+
+        return config;
     }
 
 }
